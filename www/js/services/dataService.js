3d81379cead9efe01c5ea7f7cd7b06df25f75b7f
Diego <diego.peralta.dev@gmail.com>
2015-10-22 00:41:34 -0300

    Adds remote db replica

--- www/js/services/dataService.js
+++ www/js/services/dataService.js
@@ -2,6 +2,7 @@ angular.module('preciosa')
 .service('DataService', function(pouchCollection) {
 
   var data = pouchCollection('productos');
+  data.$db.replicate.sync('http://127.0.0.1:5984/productos', {live: true});
 
   return {
     getData: getData,
